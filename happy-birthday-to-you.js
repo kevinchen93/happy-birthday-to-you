e9b2a1a224d9f1eb40c440a9dd75e54ab59b054f
Kevin Chen <xkevinchen@gmail.com>
2018-05-12 05:54:09 -0400

    initial commit

---
+++ happy-birthday-to-you.js
@@ -0,0 +1,15 @@
+/* 
+
+------ STEPS ------
+1.  Ensure that name is in correct format (only strings, also account for 		special characters).
+
+1a. Check user input birthday format (in MMDDYYYY) and alert if incorrect 	    format
+
+1b. Also ensure that a maximum length of 8 characters is allowed for input	 	 birthday.
+
+
+2. Once correct date format is inputted, compare it to table of users.
+
+3. Play a birthday song if dates match; otherwise, a Rick Roll.
+
+*/
